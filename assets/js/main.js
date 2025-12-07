49beba3d2568f441c058f8340b96c279a809c838
mohammadmehdi <mohammadmehdiz4455@gmail.com>
2025-12-07 13:35:14 +0330

    FEAT: add responsive header with hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,12 @@
-/*=============== SHOW MENU ===============*/
+const toggleBtn = document.querySelector("#toggle")
+const nav__list = document.querySelector(".nav__list")
+const closeBtn = document.querySelector("#close")
+toggleBtn.addEventListener("click" ,()=>{
+    nav__list.classList.toggle("open")
+    closeBtn.classList.toggle("open")
+})
 
-
-/*=============== REMOVE MENU MOBILE ===============*/
-
-
-/*=============== SWIPER HOME ===============*/ 
-
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+closeBtn.addEventListener("click" , ()=>{
+    nav__list.classList.remove("open")
+    closeBtn.classList.remove("open")
+})
