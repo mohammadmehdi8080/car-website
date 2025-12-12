6fb061a76ae98e258f9b9400ad934974a867c19e
mohammadmehdi <mohammadmehdiz4455@gmail.com>
2025-12-12 10:30:31 +0330

    feat(home): make section responsive, fix menu bug and integrate slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,32 @@
-const toggleBtn = document.querySelector("#toggle")
-const nav__list = document.querySelector(".nav__list")
-const closeBtn = document.querySelector("#close")
-toggleBtn.addEventListener("click" ,()=>{
-    nav__list.classList.toggle("open")
-    closeBtn.classList.toggle("open")
-})
+const toggleBtn = document.querySelector("#toggle");
+const navList = document.querySelector(".nav__list");
+const closeBtn = document.querySelector("#close");
 
-closeBtn.addEventListener("click" , ()=>{
-    nav__list.classList.remove("open")
-    closeBtn.classList.remove("open")
-})
+toggleBtn.addEventListener("click", () => {
+    navList.classList.toggle("open");
+    closeBtn.classList.toggle("open");
+});
+
+closeBtn.addEventListener("click", () => {
+    navList.classList.remove("open");
+    closeBtn.classList.remove("open");
+});
+
+const swiper = new Swiper(".home__swiper", {
+    loop: true,                // چرخش بی‌نهایت
+    slidesPerView: 1,          // تعداد اسلایدهای قابل نمایش
+    spaceBetween: 0,           // فاصله بین اسلایدها
+    autoplay: {                // حرکت خودکار
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {              // صفحه‌بندی
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    effect: "fade",            // افکت انتقال (fade برای یک اسلاید)
+    fadeEffect: {
+        crossFade: true
+    },
+    speed: 600,                // سرعت انیمیشن
+});
